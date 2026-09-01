068282d776227755a7227b5cd2b4e00c2c3b22e1
JuanPP318 <juanppenela@gmail.com>
2026-09-01 20:45:15 -0300

    subo todo

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,46 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.lang.annotation.Retention;
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private int poder;
+    private String tipo;
+
+    public Artefacto(String nombre, int poder, String tipo){
+        this.nombre = nombre;
+        this.poder = poder;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getPoder() {
+        return this.poder;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return  false;
+        }
+        Artefacto nombreArtefacto = (Artefacto) o;
+        return Objects.equals(nombre, nombreArtefacto.nombre);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(nombre, poder, tipo);
+    }
+
 }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,53 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+    //private Artefacto artefacto;
+
+    Set<Artefacto> catalogo = new HashSet<>();
+
+    List<Artefacto> tipoDeArtefactos = new ArrayList<>();
+
+    Map<String, Integer> cantidadDeArtefactosPorTipo = new HashMap<>();
+
+    public void agregarArtefacto(Artefacto artefacto){
+         catalogo.add(artefacto);
+         tipoDeArtefactos.add(artefacto);
+
+    }
+
+    public Set obtenerArtefactosUnicos(){
+        return catalogo;
+    }
+
+    public List buscarArtefactosPorTipo(String tipo){
+
+        return tipoDeArtefactos;
+    }
+
+    public Map<String, Integer> contarArtefactosPorTipo() {
+        int contador = 0;
+        Map<String, Integer> mapeo;
+        for(Artefacto tipoAct : catalogo){
+            tipoAct.getTipo();
+            //mapeo<
+        }
+        return mapeo;
+    }
+
+    public Artefacto obtenerArtefactoMasPoderoso(){
+        if (tipoDeArtefactos.size() == 0){
+            return null;
+        }
+        int poderAct = tipoDeArtefactos.get(0).getPoder();
+        Artefacto artMayor = tipoDeArtefactos.get(0);
+        for(Artefacto artefactoActual : tipoDeArtefactos){
+            if (artefactoActual.getPoder() >= poderAct){
+                poderAct = artefactoActual.getPoder();
+                artMayor = artefactoActual;
+            }
+        }
+        return artMayor;
+    }
 }
